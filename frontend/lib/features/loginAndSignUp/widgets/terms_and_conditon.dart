f31d63e1df7c49633ac5fe26d5d64870136544f4
vara-6262 <manuelvaragona@gmail.com>
2025-11-20 16:14:52 +0100

    fix checkbox bug

--- frontend/lib/features/loginAndSignUp/widgets/terms_and_conditon.dart
+++ frontend/lib/features/loginAndSignUp/widgets/terms_and_conditon.dart
@@ -1,28 +1,39 @@
-import 'package:aurabus/routing/router.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class Termsandconditon extends StatelessWidget {
+class Termsandconditon extends StatefulWidget {
   const Termsandconditon({super.key});
+  
+  @override
+  State<StatefulWidget> createState()   => _Termsandconditon();
+}
+
+class _Termsandconditon extends State<Termsandconditon>
+{
+      bool isChecked = false;
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.only(left: 20,right:30),
+    margin: const EdgeInsets.only(left: 20,right:30),
     child:Row
       (
         mainAxisAlignment: MainAxisAlignment.center,
         children: 
         [
-          Checkbox(value: false, onChanged:(bool? value) {context.go(AppRoute.login);},),
+          Checkbox(value: isChecked, onChanged:(bool? value) {
+              setState(() {
+                isChecked = value ?? false;
+              });
+          }),
+          
           Expanded(
             child: Text(
             "I Agree with the Terms and Conditions and Privacy Policy",
             style:GoogleFonts.inter(fontSize: 10),)
-        )
+        ) 
         ],
-      ));
-
+      )
+    );
   }
 }
 
